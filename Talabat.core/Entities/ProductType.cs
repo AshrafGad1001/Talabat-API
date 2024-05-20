404e4434b3ada5e116fcbbea5f6b27d6a3a8047d
Ashraf Gad <158779399+AshrafGad1001@users.noreply.github.com>
2024-05-20 18:04:46 +0300

    ..

--- Talabat.core/Entities/ProductType.cs
+++ Talabat.core/Entities/ProductType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Talabat.core.Entities
+﻿namespace Talabat.core.Entities
 {
     public class ProductType:BaseEntity
     {
